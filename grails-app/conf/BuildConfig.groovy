a4686be9803f0b23add4215842cf16d83670266a
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-10-07 15:32:08 +0200

    Essaie d'une nouvelle configuration de sonarqube pour supporter le CodeCoverage #16

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -74,6 +74,7 @@ grails.project.dependency.resolution = {
 
 coverage {
     xml = true
+    sourceInclusions = ['grails-app/domain', 'grails-app/services', 'grails-app/controllers']
 }
 
 codenarc.reports = {
