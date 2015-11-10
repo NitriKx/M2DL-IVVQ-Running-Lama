21bb2734752cc0acf3509531571d1a2e19582124
Julien Custoja <julien.custoja@gmail.com>
2015-11-10 18:41:23 +0100

    Augmente la couverture de code

--- test/integration/com/runninglama/TrajetDAOServiceSpec.groovy
+++ test/integration/com/runninglama/TrajetDAOServiceSpec.groovy
@@ -32,36 +32,51 @@ class TrajetDAOServiceSpec extends Specification {
         Trajet.findAllById(resultatAjout.id).size() == 1
     }
 
-//    void "teste que la méthode search retourne bien une liste de trajet de la base de données"() {
-//        given: "plusieurs trajets valides en base de données"
-//        def utilisateur = TestsHelper.creeUtilisateurValide();
-//        utilisateur = utilisateur.save(flush: true)
-//        def vehicule = TestsHelper.creeVehiculeValide(utilisateur);
-//        vehicule = vehicule.save(flush: true)
-//        def trajet = TestsHelper.creeTrajetValide(utilisateur, vehicule);
-//        trajet.save(flush: true)
-//        def trajet2 = TestsHelper.creeTrajetValide2(utilisateur, vehicule);
-//        trajet2.save(flush: true)
-//        def trajet3 = TestsHelper.creeTrajetValide3(utilisateur, vehicule);
-//        trajet3.save(flush: true)
-//
-//        trajetDAOService.save(trajet)
-//        trajetDAOService.save(trajet2)
-//        trajetDAOService.save(trajet3)
-//
-//        when: "on demande la recherche d'un trajet"
-//        def params
-//        params['depart_google']     = 'okinawa'
-//        params['arrivee_google']    = 'osaka'
-//        params['prixMax']           = 100
-//        params['dateAller']         = new Date()
-//        params['dateRetour']        = new Date()
-//        def resultatRecherche = trajetDAOService.search(params)
-//
-//        then: "le trajet existe dans la base de données"
-//        resultatRecherche != null
-//        resultatRecherche.size() == 2
-//    }
+    void "test la methode trouver"() {
+        given: "un trajet valide pas en base de données"
+        def utilisateur = TestsHelper.creeUtilisateurValide();
+        utilisateur = utilisateur.save(flush: true)
+        def vehicule = TestsHelper.creeVehiculeValide(utilisateur);
+        vehicule = vehicule.save(flush: true)
+        def trajet = TestsHelper.creeTrajetValide(utilisateur, vehicule);
+        trajet.save(flush: true)
+
+        when: "on demande a récupérer le trajet"
+        def resultatAjout = trajetDAOService.save(trajet)
+
+        then: "le trajet existe dans la base de données"
+        trajetDAOService.trouver(trajet.id) != null
+
+    }
+
+    void "teste que la méthode search retourne bien une liste de trajet de la base de données"() {
+        given: "plusieurs trajets valides en base de données"
+        def utilisateur = TestsHelper.creeUtilisateurValide();
+        utilisateur = utilisateur.save(flush: true)
+        def vehicule = TestsHelper.creeVehiculeValide(utilisateur);
+        vehicule = vehicule.save(flush: true)
+
+        def trajet = TestsHelper.creeTrajetValide(utilisateur, vehicule);
+        def trajet2 = TestsHelper.creeTrajetValide2(utilisateur, vehicule);
+        trajet2.save(flush: true)
+        def trajet3 = TestsHelper.creeTrajetValide3(utilisateur, vehicule);
+        trajet3.save(flush: true)
+
+        trajetDAOService.save(trajet)
+        trajetDAOService.save(trajet2)
+        trajetDAOService.save(trajet3)
+
+        when: "on demande la recherche d'un trajet"
+        def params = new HashMap<String, Object>()
+        params['depart_google']     = 'okinawa'
+        params['arrivee_google']    = 'osaka'
+
+
+        def resultatRecherche = trajetDAOService.search(params)
+        then: "le trajet existe dans la base de données"
+        resultatRecherche != null
+        resultatRecherche.size() == 2
+    }
 
 
 }
