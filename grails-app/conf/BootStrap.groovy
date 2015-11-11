319a363d094205fcb5e2b331302b0c75125ebb1a
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-11-11 08:57:07 +0100

    Ajout d'un utilisateur supplémentaire (avec le mot de passe en commentaire cette fois :p)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,7 +15,15 @@ class BootStrap {
                 passwordHash: '49440ae7cec983ae6155b1d4d80ee77bd071b115', passwordSalt: 'abc', dateInscription: new Date(),
                 dateDerniereConnexion: new Date(), dateNaissance: new Date(), telephone: "0987675434", vehicules: []).save(flush: true)
 
+        // Mot de passe : abcde
+        Utilisateur utilisateur3 = new Utilisateur(email: 'email3@test.fr', nom: "Benoit", prenom: "Sauvere", pseudo: "NitriKx",
+                passwordHash: 'fe1a276b6e561c7fd842b442e164bce1d564b3ac', passwordSalt: 'abc', motDePasse: "abcde", motDePasseConfirmation: "abcde",
+                dateInscription: new Date(), dateDerniereConnexion: new Date(), dateNaissance: new Date(), telephone: "0987675434", vehicules: []).save(flush: true)
+
         Vehicule vehicule = new Vehicule(possesseur: utilisateur1, marque: "Telsa", modele: "Model X", annee: new Date(year: 2015), nb_place: 5,
+                kilometrage: 10000, type: TypeVehicule.VOITURE).save(flush: true);
+
+        Vehicule vehicule2 = new Vehicule(possesseur: utilisateur2, marque: "Citroen", modele: "C4", annee: new Date(year: 2015), nb_place: 5,
                 kilometrage: 10000, type: TypeVehicule.VOITURE).save(flush: true);
 
         Trajet trajet = new Trajet(conducteur: utilisateur1, vehicule: vehicule, commentaire: "", prix: 29.99, nombrePlace: 3, dateAller: new Date(), dateRetour: new Date(),
